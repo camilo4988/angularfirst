e1dce36ad051e5cc8121ddbcb4a5214bab106323
ingcamilolopez2017@gmail.com <ingcamilolopez2017@gmail.com>
2025-06-06 09:03:42 -0500

    primero angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ComponenteUnoComponent} from './components/componente-uno/componente-uno.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ComponenteUnoComponent,FormsModule],
+  //template:'<h1>hola inmundo</h1>',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pry_test';
+  //creo variable
+  username: string='Camilo';
+  number: number=0;
+
+  condicion1:boolean=false;
+
+  addOne(){
+    this.number += 1;
+  }
 }
---
+++ src/app/components/componente-uno/componente-uno.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-uno',
+  imports: [],
+  templateUrl: './componente-uno.component.html',
+  styleUrl: './componente-uno.component.css'
+})
+export class ComponenteUnoComponent {
+
+}
